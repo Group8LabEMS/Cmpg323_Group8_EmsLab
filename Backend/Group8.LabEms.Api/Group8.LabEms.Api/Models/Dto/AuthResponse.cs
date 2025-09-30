b0f08f995de860e77915b43bde82fc073e61bf6d
M-kellerman <Markram.Kellerman@gmail.com>
2025-09-30 17:50:16 +0200

    Revert "Merge branch 'development' of https://github.com/M-kellerman/Cmpg323_Group8_EmsLab into development"
    This reverts commit e1ee85a8c42c90d5cd4a80d219d248a29ffc34ab, reversing
    changes made to f7671841eaf65526ca53ebe26f96b579fd4a7adc.

--- Backend/Group8.LabEms.Api/Group8.LabEms.Api/Models/Dto/AuthResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace Group8.LabEms.Api.Models.Dto
-{
-    public class AuthResponse
-    {
-        public string Token { get; set; } = null!;
-        public DateTime ExpiresAt { get; set; }
-    }
-}
